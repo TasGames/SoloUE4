7b5d217bb80a6134fbe134b9f26126277f964ff4
Thomas Simon <thomas.simon@btopenworld.com>
2019-02-15 02:35:01 +0000

    Created project

---
+++ CrazyTaxiProcGen/Intermediate/Build/Win64/UE4Editor/Inc/CrazyTaxiProcGen/CrazyTaxiProcGenGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CRAZYTAXIPROCGEN_CrazyTaxiProcGenGameModeBase_generated_h
+#error "CrazyTaxiProcGenGameModeBase.generated.h already included, missing '#pragma once' in CrazyTaxiProcGenGameModeBase.h"
+#endif
+#define CRAZYTAXIPROCGEN_CrazyTaxiProcGenGameModeBase_generated_h
+
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_RPC_WRAPPERS
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACrazyTaxiProcGenGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrazyTaxiProcGenGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrazyTaxiProcGenGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/CrazyTaxiProcGen"), NO_API) \
+	DECLARE_SERIALIZER(ACrazyTaxiProcGenGameModeBase)
+
+
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACrazyTaxiProcGenGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrazyTaxiProcGenGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrazyTaxiProcGenGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/CrazyTaxiProcGen"), NO_API) \
+	DECLARE_SERIALIZER(ACrazyTaxiProcGenGameModeBase)
+
+
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrazyTaxiProcGenGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrazyTaxiProcGenGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrazyTaxiProcGenGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrazyTaxiProcGenGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrazyTaxiProcGenGameModeBase(ACrazyTaxiProcGenGameModeBase&&); \
+	NO_API ACrazyTaxiProcGenGameModeBase(const ACrazyTaxiProcGenGameModeBase&); \
+public:
+
+
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrazyTaxiProcGenGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrazyTaxiProcGenGameModeBase(ACrazyTaxiProcGenGameModeBase&&); \
+	NO_API ACrazyTaxiProcGenGameModeBase(const ACrazyTaxiProcGenGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrazyTaxiProcGenGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrazyTaxiProcGenGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrazyTaxiProcGenGameModeBase)
+
+
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_12_PROLOG
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_RPC_WRAPPERS \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_INCLASS \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CrazyTaxiProcGen_Source_CrazyTaxiProcGen_CrazyTaxiProcGenGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
