ecd349340f58f48df542f2e64e351a5617ea3acf
Thomas Simon <thomas.simon@btopenworld.com>
2019-02-22 08:43:06 +0000

    Spawns cells and adds them to array

--- CrazyTaxiProcGen/Source/CrazyTaxiProcGen/GenerateGrid.cpp
+++ CrazyTaxiProcGen/Source/CrazyTaxiProcGen/GenerateGrid.cpp
@@ -2,6 +2,7 @@
 
 #include "GenerateGrid.h"
 
+
 AGenerateGrid::AGenerateGrid()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,6 +13,7 @@ void AGenerateGrid::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	SetPositions();
 }
 
 void AGenerateGrid::Tick(float DeltaTime)
@@ -22,9 +24,33 @@ void AGenerateGrid::Tick(float DeltaTime)
 
 void AGenerateGrid::SetPositions()
 {
-	for (int i = 0; i < 31; i++)
+	FActorSpawnParameters SpawnParams;
+	FVector spawnLoc = FVector(0.0f, 0.0f, 0.0f);
+	FRotator spawnRot = FRotator::ZeroRotator;
+
+	int column = 1;
+	int row = 1;
+
+	for (int j = 0; j < 31; j++)
 	{
-		ACell* Cell = ArrayOfCells[i];
+		for (int i = 0; i < 31; i++)
+		{
+			ACell* cell = GetWorld()->SpawnActor<ACell>(cellClass, spawnLoc, spawnRot, SpawnParams);
+			ArrayOfCells.Add(cell);
+
+			cell->SetPositionX(spawnLoc.X);
+			cell->SetPositionY(spawnLoc.Y);
+			cell->SetRow(row);
+			cell->SetColumn(column);
+
+			spawnLoc.X += 800;
+			row += 1;
+		}
+
+		spawnLoc.X = 0;
+		row = 1;
+		spawnLoc.Y += 800;
+		column += 1;
 
 	}
 }
